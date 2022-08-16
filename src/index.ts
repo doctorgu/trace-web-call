1d70c6393fca92a07ed0fa050e91e5c02cf18d30
doctorgu <doctorgu@naver.com>
2022-08-16 14:58:49 +0900

    Old source which is not using java-parser deleted

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from 'fs';
 import { findFiles } from './common/util';
 import { getClassInfo } from './common/classHelper';
+import { getXmls } from './common/xmlHelper';
 
 async function getControllerInfo(rootDir: string, filePattern: string) {
   const callerOnlyInVars = true;
@@ -20,11 +21,19 @@ async function getServiceImplInfo(rootDir: string, filePattern: string) {
     console.log(fullPath);
   }
 }
+async function getXmlIds(rootDir: string, filePattern: string) {
+  for await (const fullPath of findFiles(rootDir, filePattern)) {
+    const xml = readFileSync(fullPath, 'utf-8');
+    const xmls = getXmls(xml);
+    console.log(JSON.stringify(xmls, null, '  '));
+  }
+}
 // getControllerInfo(
 //   'D:\\Temp\\kbbizmicro-sb\\bz-manual-api-common\\src\\main\\java\\biz\\micro\\portal\\manual\\api\\common\\controller',
 //   'ManualEditorController.java'
 // );
-getServiceImplInfo(
-  'D:\\Temp\\kbbizmicro-sb\\bz-manual-api-common\\src\\main\\java\\biz\\micro\\portal\\manual\\api\\common\\spring\\service',
-  'ManualServiceImpl.java'
-);
+// getServiceImplInfo(
+//   'D:\\Temp\\kbbizmicro-sb\\bz-manual-api-common\\src\\main\\java\\biz\\micro\\portal\\manual\\api\\common\\spring\\service',
+//   'ManualServiceImpl.java'
+// );
+getXmlIds('D:\\Temp\\kbbizmicro-sb\\bz-manual-api-common\\src\\main\\resources\\sql\\oracle', 'DocNotice.xml');
